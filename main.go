f59ae0f623f675c87ba293f9d2e5d32c6986e7e7
towa1204 <lsadsfj@gmail.com>
2024-01-14 00:39:02 +0900

    出力先ディレクトリを指定できるようにした

--- main.go
+++ main.go
@@ -5,14 +5,19 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 )
 
 func main() {
 	var fileName = flag.String("f", "hoge.drawio", "specify drawio filename")
+	var outDirName = flag.String("o", ".", "specify output directory")
 	flag.Parse()
 	if *fileName == "hoge.drawio" {
 		log.Fatal("ファイル名を指定してください")
+	}
+	if !isExistDir(*outDirName) {
+		log.Fatal("そのようなディレクトリは存在しません")
 	}
 
 	drawioPath, err := chooseDrawioPath()
@@ -29,15 +34,16 @@ func main() {
 
 	baseFileName := getFileNameWithoutExt(*fileName)
 	for i := 0; i < drawioFile.Pages; i++ {
-		pageNumber := strconv.Itoa(i + 1)
-		outputFileName := fmt.Sprintf("%s-%s.png", baseFileName, pageNumber)
-		cmd := exec.Command(drawioPath, "-x", "-f", "png", "-o", outputFileName, "-p", pageNumber, *fileName)
-		err = cmd.Run()
+		pageNumber := strconv.Itoa(i)
+		outFileName := filepath.Join(*outDirName, fmt.Sprintf("%s-%d.png", baseFileName, i+1))
+
+		cmd := exec.Command(drawioPath, "-x", "-f", "png", "-o", outFileName, "-p", pageNumber, *fileName)
+		err := cmd.Run()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("exported %s -> %s\n", drawioFile.Diagrams[i].Name, outputFileName)
+		fmt.Printf("exported %s -> %s\n", drawioFile.Diagrams[i].Name, outFileName)
 	}
 
 }
--- utils.go
+++ utils.go
@@ -1,7 +1,28 @@
 package main
 
-import "path/filepath"
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
 
 func getFileNameWithoutExt(path string) string {
 	return filepath.Base(path[:len(path)-len(filepath.Ext(path))])
+}
+
+// isExistDir は与えられた文字列がディレクトリとして存在するか判定する
+func isExistDir(dirName string) bool {
+	absPath, err := filepath.Abs(dirName)
+	if err != nil {
+		return false
+	}
+
+	fileInfo, err := os.Stat(absPath)
+
+	if !errors.Is(err, fs.ErrNotExist) && err == nil && fileInfo.IsDir() {
+		return true
+	}
+
+	return false
 }
